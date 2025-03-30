25543b497f26685a82f64f8cbb4db817cb942c80
Harry (hjbdev) <harry@hjb.dev>
2025-03-30 15:40:45 +0000

    docker install

--- provision.sh
+++ provision.sh
@@ -107,6 +107,31 @@ bun add -g pm2
 ln -s $(which bun) /usr/bin/node # (official) workaround for not having node
 $(pm2 startup | sed 's/^sudo //') | su - keystone -c "bash"
 
+# Install Docker Engine
+apt_wait
+apt-get -y remove docker docker-engine docker.io containerd runc
+apt_wait
+apt-get update
+apt_wait
+apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+apt_wait
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt_wait
+apt-get -y install docker-ce docker-ce-cli containerd.io
+apt_wait
+# end docker install
+
 # Setup Unattended Security Upgrades
 apt-get install -y --force-yes unattended-upgrades
 cat >/etc/apt/apt.conf.d/50unattended-upgrades <<EOF
