b68720b1252cd927258d625585b2c91a8e6ada92
Harry (hjbdev) <harry@hjb.dev>
2025-03-31 16:18:41 +0000

    morph map

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+use App\Models\Application;
+use App\Models\Deployment;
+use App\Models\Environment;
+use App\Models\Organisation;
+use App\Models\OrganisationUser;
+use App\Models\Server;
+use App\Models\Service;
+use App\Models\Slice;
+use App\Models\Step;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
@@ -22,6 +31,15 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Relation::enforceMorphMap([
+            'application' => Application::class,
+            'deployment' => Deployment::class,
+            'environment' => Environment::class,
+            'organisation' => Organisation::class,
+            'organisation-user' => OrganisationUser::class,
+            'server' => Server::class,
+            'service' => Service::class,
+            'slice' => Slice::class,
+            'step' => Step::class,
             'user' => User::class,
         ]);
     }
