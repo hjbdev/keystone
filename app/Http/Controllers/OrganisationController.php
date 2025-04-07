5d042900ae4c05a5ab54ee63032d614b707819af
Harry (hjbdev) <harry@hjb.dev>
2025-04-07 19:59:30 +0100

    added tabs component, org show wip

--- app/Http/Controllers/OrganisationController.php
+++ app/Http/Controllers/OrganisationController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Provider;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
 class OrganisationController extends Controller
 {
-    public function show()
+    public function show(Request $request)
     {
-        return inertia('organisations/Show');
+        return inertia('organisations/Show', [
+            'providers' => Inertia::lazy(fn () => Provider::whereOrganisationId($request->route('organisation'))->get()),
+        ]);
     }
 }
--- resources/js/pages/organisations/Show.vue
+++ resources/js/pages/organisations/Show.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import AppLayout from '@/layouts/AppLayout.vue';
-import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
-import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import { Head, WhenVisible } from '@inertiajs/vue3';
 
-const props = defineProps({
+defineProps({
     organisation: {
         type: Object,
+        required: true,
+    },
+    providers: {
+        type: Array,
         required: true,
     },
 });
@@ -17,7 +20,24 @@ const props = defineProps({
 
     <AppLayout>
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-            {{ organisation }}
+            <h2 class="text-3xl font-bold tracking-tight">{{ organisation.name }}</h2>
+            <Tabs default-value="dashboard" class="w-[400px]">
+                <TabsList>
+                    <TabsTrigger value="dashboard"> Dashboard </TabsTrigger>
+                    <TabsTrigger value="settings"> Settings </TabsTrigger>
+                </TabsList>
+                <TabsContent value="dashboard"> Overview on organisation. </TabsContent>
+                <TabsContent value="settings"> 
+                    <WhenVisible data="providers">
+                        <template #fallback>
+                            Loading...
+                        </template>
+                        <h3 class="text-2xl font-bold tracking-tight mt-4">Server Providers</h3>
+                        <p class="text-sm text-muted-foreground">Manage your server providers.</p>
+                        
+                    </WhenVisible>
+                </TabsContent>
+            </Tabs>
         </div>
     </AppLayout>
 </template>
